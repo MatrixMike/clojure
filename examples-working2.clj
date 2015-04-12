6babb1c608272b84626aeeffed587c0f39e71701
Mike Hewitt <MatrixMike@users.noreply.github.com>
2015-04-12 22:06:39 +1000

    Create examples-working2.clj

---
+++ examples-working2.clj
@@ -0,0 +1,7 @@
+(for [i (range 10)] (* i i ))
+;; Sun Apr 12 21:16:54 EST 2015 
+
+
+(for [i (range 4)]  (str "set " "00"i   " red ;" )        
+
+(for [i (range 4)]  (spit  "cube2.txt"  (str  "set " i"00" " red; \n"):append true))
