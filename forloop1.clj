ec1504078b9fb9c7e9bda30d6e3501f457b5e19b
Mike Hewitt <MatrixMike@users.noreply.github.com>
2015-04-15 22:01:13 +1000

    Create forloop1.clj
    Demo of for loop with seq  - part of bike gear project - see scala example in this git

---
+++ forloop1.clj
@@ -0,0 +1,39 @@
+(def digits (seq [1 2 3]))
+(for [x1 digits x2 digits] (* x1 x2))
+;
+
+
+;15.04.2015
+(def fgear (seq [22.0 32.0 42.0]))
+(def rgear (seq [26 23 21 19 17 15 13 12]))
+
+(for [fgear digits1 rgear digits2] (/ fgear rgear))
+;
+; now to sort the created list
+
+;; next version 
+(for [x ['a 'b 'c] 
+      y [1 2 3]]
+  [x y])
+  
+  ; https://clojuredocs.org/clojure.core/for
+  
+(for [x [22.0 32.0 42.0] 
+      y [26 23 21 19 17 15 13 12]]
+  [x y])
+  
+    ;; maybe we can feed the sequence into something and divide 
+    ;; one value by the other;15.04.2015
+(def fgear (seq [22.0 32.0 42.0]))
+(def rgear (seq [26 23 21 19 17 15 13 12]))
+
+(for [fgear digits1 rgear digits2] (/ fgear rgear))
+;
+
+ ;15.04.2015
+(def fgear (seq [32.0 22.0 42.0]))
+(def rgear (seq [26 23 21 19 17 15 13 12]))
+
+(for [f fgear r rgear ] (/ f r))  ;; creates unsorted output
+;
+   
