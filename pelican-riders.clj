3ee484806d403a9c42dfa1da9fc33d908ea8cefd
Mike Hewitt <MatrixMike@users.noreply.github.com>
2015-04-16 01:42:50 +1000

    Create pelican-riders.clj

---
+++ pelican-riders.clj
@@ -0,0 +1,31 @@
+(ns tutorial.core
+(:require   [clojure.string :as cstr]
+            [clojure.set :as cset]))
+
+(def riders ["mike" "graeme" "piet" " " "steve" "" "heinz" "23" "Colin"])
+
+(defn uppercase-first "Returns word with first letter in upper case" 
+	[word]
+	(if-not (or (empty? word) (nil? word))
+	(cstr/join (cons (cstr/upper-case (first word))
+		(rest word)))
+		word))
+		
+(def greeting "Hello")
+		
+(defn hello 
+	"Returns hello string" 
+	[who]
+	(str greeting  " " who "!" ))
+				
+(defn hello-riders 
+	"Returns greetings to all the riders."
+	[]
+	(hello (cstr/join ", " (map uppercase-first  (reverse (sort riders))))))
+				
+(hello-riders)
+
+;; ( (first greeting) (last greeting))
+
+;; (cstr/join ( cons (first greeting) (last greeting)))
+
