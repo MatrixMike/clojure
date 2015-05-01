7abe3322a8e9191133f3c8535107e478769562a6
Mike Hewitt <MatrixMike@users.noreply.github.com>
2015-05-01 11:18:13 +1000

    Update javax-clojure2.clj
    added a call to date function
    still a bit messy - and still learning more about git - however the tools are helping me already

--- javax-clojure2.clj
+++ javax-clojure2.clj
@@ -6,6 +6,9 @@
 ;; (str "Hello" User)
 ;; 26.04.2015 22:35:15
 
+(defn now [] (new java.util.Date))
+
+
 ;; this can be executed from the command line    clojure javax-clojure2.clj  
 (def prop8 "os.arch") 	;;Operating system architecture
 (def prop9 "os.name") 	;;Operating system name
@@ -23,12 +26,16 @@
 (def p6 (System/getProperty prop6))
 (def pos (System/getProperty ppos))
 
-
+// some help from http://stackoverflow.com/questions/4635680/what-is-the-best-way-to-get-date-and-time-in-clojure
 (javax.swing.JOptionPane/showMessageDialog nil 
-	(str "Hello " userN "\n" "Your Home Dir is " userH "\nRunning " p8 " " p9 "\nJava " p6 "\nOS " pos ) )  
+	(str "Hello " userN "\n" "Your Home Dir is " userH "\nRunning " p8 " " p9 "\nJava " p6 "\nOS " pos "\n" (now)) )  
 		; prints hello and the user name 
 
 ;; http://docs.oracle.com/javase/7/docs/api/javax/swing/JOptionPane.html
+
+
+
+
 
 
 ;; this can be executed from the command line    clojure javax-clojure2.clj  
@@ -43,9 +50,11 @@
 (def userH (System/getProperty UH))
 (def p8 (System/getProperty prop8))
 (def p9 (System/getProperty prop9))
-(javax.swing.JOptionPane/showMessageDialog nil (str "Hello " userN "\n" "Your Home Dir is " userH "\nRunning " p8 " " p9) )  ; prints hello and the user name 
+(javax.swing.JOptionPane/showMessageDialog nil (str "Hello " userN "\n" "Your Home Dir is " userH "\nRunning " p8 " " p9 "\n" (now)) )  ; prints hello and the user name 
 
 ;; http://docs.oracle.com/javase/7/docs/api/javax/swing/JOptionPane.html
+
+
 
 
 
