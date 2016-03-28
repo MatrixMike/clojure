149b520b978f1f1e7b1401dc8fee6fec6c9a973c
MatrixMike <matroxmike@icloud.com>
2016-03-28 11:18:04 +1100

    stages in proguard addition to project

---
+++ proguard/project.clj
@@ -0,0 +1,12 @@
+(defproject proguard "0.1.0-SNAPSHOT"
+  :description "Testing proguard sample from web"
+  :url "http://http://stackoverflow.com/questions/12281365/obfuscating-clojure-uberjars-with-proguard"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :aot :all
+  :main proguard.core
+  
+  )
+  
+  
