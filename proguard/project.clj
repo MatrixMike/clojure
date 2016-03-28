0a2b1d76efc03b723e9b0fc21efded3a2c2f1231
MatrixMike <matroxmike@icloud.com>
2016-03-28 11:21:45 +1100

    lein detects duplicate entries

--- proguard/project.clj
+++ proguard/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :aot :all
+
   :main proguard.core
 ;;; Make sure everything is aot compiled
   :aot :all
