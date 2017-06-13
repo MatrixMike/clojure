434af1865b8f05d518d3638fa8ee590f3d4c24d4
MatrixMike <matroxmike@icloud.com>
2017-06-13 19:38:22 +1000

    fix up stackoverflow

--- stringeye/project.clj
+++ stringeye/project.clj
@@ -1,4 +1,4 @@
-(defproject stringeye "0.1.1-SNAPSHOT"
+(defproject stringeye "0.1.2-SNAPSHOT"
   :description "stringeye"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
