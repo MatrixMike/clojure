453ab019b08dbc6fea2275772ad6814f6ead172f
Mike J Hewitt <mikeh@electroteach.com>
2017-09-21 13:38:57 +1000

    formatting issues

--- bikegears/src/bikegears/core.clj
+++ bikegears/src/bikegears/core.clj
@@ -10,11 +10,9 @@
 (def fgear (seq [22.0 32.0 42.0]))
 (def rgear (seq [26 23 21 19 17 15 13 12])) 
  ;;(doseq  [n (range  3 010 2 )]    (sort (for [f fgear r rgear ] (/ f r))  ) ;;  now a sorted list 22/2/2016
- (print "%3.3f v" [(sort (for [f fgear r rgear ] (/ f r))  )]
-
- )
- 
+ (println "%3.3f v" [(sort (for [f fgear r rgear ] (/ f r))  )])
+ (printf "1 + 2 is %s%n" 3)
  
 (defn -main [& args]
-(println "test 1")
+(printf "1 + 2 is %s%n" 3)
 )
