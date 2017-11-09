89de1ba19b33e574de4bdb46181cc3512726b0fb
MatrixMike <matroxmike@icloud.com>
2017-11-10 10:49:18 +1100

    more commas in lists

--- bikegears/src/bikegears/core.clj
+++ bikegears/src/bikegears/core.clj
@@ -7,7 +7,7 @@
 ;;  "I don't do a whole lot."
  ;; [x]
  ;; (println x "Hello, World!"))
-(def fgear (seq [22.0 32.0 42.0]))
+(def fgear (seq [22.0, 32.0, 42.0]))
 ;;(def rgear (seq [26 23 21 19 17 15 13 12])) 
 (def rgear (seq [26, 23, 21, 19, 17, 15, 13, 12]))
  ;;(doseq  [n (range  3 010 2 )]    (sort (for [f fgear r rgear ] (/ f r))  ) ;;  now a sorted list 22/2/2016
