88444b09d0231f94fc9af8ce04ec8993138580df
MatrixMike <matroxmike@icloud.com>
2017-11-10 13:02:22 +1100

    fix

--- bikegears/project.clj
+++ bikegears/project.clj
@@ -1,4 +1,4 @@
-(defproject bikegears "0.1.3-SNAPSHOT"
+(defproject bikegears "0.1.4-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
