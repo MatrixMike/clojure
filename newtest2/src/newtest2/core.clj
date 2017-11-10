03ee69f7ab6bc0304ce4c1e57b1532024aa19437
MikeH <matroxmike@icloud.com>
2017-11-11 04:43:51 +1100

    phase 1 of sample

--- newtest2/src/newtest2/core.clj
+++ newtest2/src/newtest2/core.clj
@@ -1,6 +1,14 @@
-(ns newtest2.core)
+(ns newtest2.core
+(:gen-class))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+  
+;;(ns clojure.examples.hello
+;;    (:gen-class))
+
+(defn -main
+  [greetee]
+  (println (str "Hello " greetee "!")))
