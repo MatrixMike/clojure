77de2b2e2cc46a6aa72474840f128f7b2dedc651
MatrixMike <matroxmike@icloud.com>
2017-11-10 13:07:08 +1100

    fix3

---
+++ bikegears/project.clj
@@ -0,0 +1,13 @@
+(defproject bikegears "0.1.4-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+    [expresso "0.2.2"]
+   ]
+  
+  :main bikegears.core
+  :aot :all
+  :omit-source true
+  )
