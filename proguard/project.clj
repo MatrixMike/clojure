6a5d85dbeef7868239f58eef657ac5c5f9f9ba40
Phyllis <matroxmike@icloud.com>
2018-04-13 17:04:02 +1000

    enthusiasm after meetup

--- proguard/project.clj
+++ proguard/project.clj
@@ -1,8 +1,8 @@
 (defproject proguard "0.1.0-SNAPSHOT"
-  :description "Testing proguard sample from web"
-  :url "http://http://stackoverflow.com/questions/12281365/obfuscating-clojure-uberjars-with-proguard"
+  :description    "Testing proguard sample from web"
+  :url            "http://http://stackoverflow.com/questions/12281365/obfuscating-clojure-uberjars-with-proguard"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
 
   :main proguard.core
@@ -10,8 +10,8 @@
   :aot :all
 ;;; Remove source .clj files from the resulting jar
   :omit-source true
-  
-;; http://proguard.sourceforge.net/manual/usage.html  
+
+;; http://proguard.sourceforge.net/manual/usage.html
   )
-  
-  
+
+
---
+++ stringeye/src/stringeye/coreold.clj
@@ -0,0 +1,40 @@
+(ns stringeye.core
+ (:gen-class)
+;;  (:import utils.demo)
+  (:import (javax.swing JPanel JFrame)
+           (javax.swing SwingUtilities)
+           (java.awt Dimension)
+           (java.awt.Color)
+       ;;     (utils.demo)
+            )
+)         
+; Date  13.05.2015 14:24:59
+ ;; Fri 18 Mar 2016 01:17:34 AEDT 
+(def upperR 400) (def lowerR 0) (def step 5)
+
+(defn make-panel []
+  (let [panel (proxy [JPanel] []
+                (paintComponent [g]
+                 (doseq [j (range lowerR upperR step)] (.drawLine g j 0        0      (- upperR j)))
+                 (doseq [j (range lowerR upperR step)] (.drawLine g j upperR   upperR (- upperR j)))
+     ))]
+    (doto panel
+      (.setPreferredSize (Dimension. (+ upperR 50) (+ upperR 50))))))
+ 
+(defn make-frame [panel]
+  (doto   (new JFrame "StringEye") ;; (JFrame.) ;;   give a title to the Window
+  (.add panel)
+   .pack
+                              ;; add close command here
+                              ;; (.setBackground (. Color YELLOW))
+                              ;;  f.getContentPane().setBackground(new Color(107, 106, 104));
+                             ;;  ( .getContentPane.setBackground     YELLOW) )
+  (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+   .show))
+ 
+(defn -main [& args] 
+
+(make-frame (make-panel)) ;; (System/exit 0)) 
+;;(println (. demo halves upperR 50))  
+)
+
