afb0164149bc919f2c2bb8e6e4edc33742853ce0
Mike H <matroxmike@icloud.com>
2018-10-14 17:44:10 +1100

    added colour on RPi @ PB

---
+++ string-eye2.clj
@@ -0,0 +1,31 @@
+;;
+(ns drawing-demo
+  (:import [javax.swing JPanel JFrame]
+           [java.awt Dimension]
+           [java.awt Color]
+))
+;           
+; Date  13.05.2015 14:24:59
+ 
+(def upperR 400) (def lowerR 0) (def step 5)
+
+(defn make-panel []
+  (let [panel (proxy [JPanel] []
+                (paintComponent [g]
+                 (doto g       (.setColor Color/BLUE)) 
+                 (doseq [j (range lowerR upperR step)] (.drawLine g j 0        0      (- upperR j)))
+                 (doseq [j (range lowerR upperR step)] (.drawLine g j upperR   upperR (- upperR j)))
+     ))]
+    (doto panel
+      (.setPreferredSize (Dimension. (+ upperR 50) (+ upperR 50))))))
+ 
+(defn make-frame [panel]
+  (doto (JFrame.)        ;;was (new JFrame)
+    (.add panel)
+    .pack
+    (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+    .show))
+ 
+(make-frame (make-panel)) ;; (System/exit 0))    
+             
+ 
